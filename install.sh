5ed8bdbced2ee31002dd4473988b845d7929f523
lisosia <rainbowseptet@gmail.com>
2016-04-21 21:43:36 +0900

    absolute make link

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd ~
-git clone https://github.com/lisosia/dotfiles.git
-echo "download done"
-cd dotfiles
+DOT_TARGET=".dotfiles"
 
+cd $HOME
+git clone https://github.com/lisosia/dotfiles.git ${DOT_TARGET}
+echo "download done"
+
+cd ${DOT_TARGET}
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     echo "$f"
-    ln -snfv "$f" "$HOME"/"$f"
+    ln -snfv ${HOME}/${DOT_TARGET}/${f} ${HOME}/${f}
 done
